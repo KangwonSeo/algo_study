4a2fa32c78beaf049bd389638a71047670e4f4f6
kangwon.seo <kangwon19@gmail.com>
2020-06-30 22:11:45 +0900

    template

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <string.h>
+#include <cstring>
+#include <map>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <utility>
+#include <numeric>
+#include <math.h>
+
+using namespace std;
+
+int main(void) {
+	int T;
+	cin >> T;
+	while (T--) {
+
+	}
+	return 0;
+}
